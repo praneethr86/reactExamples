7ae6fc672a3fba0dbffa7a7b7d61b5d6746d5333
Praneeth Reddy <praneeth001@gmail.com>
2019-08-16 18:47:05 +0530

    Adding final changes for modular code Adding webpack config Adding babelrc file config Adding JSON data in recipes.json in data folder

---
+++ recipes-app/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+import Menu from "./components/Menu";
+import data from "./data/recipes.json";
+
+render(<Menu recipes={data} />, document.getElementById("root"));
